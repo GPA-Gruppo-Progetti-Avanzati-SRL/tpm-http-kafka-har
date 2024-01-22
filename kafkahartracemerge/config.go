ea7f71c93090b765e92c11a1e285624aff8a988b
M.A.I <mario.imperato@gpagroup.it>
2024-01-22 20:38:56 +0100

    aggiornamento per filtro sulle tracce har per status-code

--- kafkahartracemerge/config.go
+++ kafkahartracemerge/config.go
@@ -10,12 +10,12 @@ type Config struct {
 	ProcessorConfig           *ProcessorConfig                 `yaml:"process,omitempty" mapstructure:"process,omitempty" json:"process,omitempty"`
 }
 
-type ConspicuousTraces struct {
+type ProminentTraces struct {
 	StatusCodeList []int `yaml:"status-code-filter,omitempty" mapstructure:"status-code-filter,omitempty" json:"status-code-filter,omitempty"`
 }
 
 type ProcessorConfig struct {
-	CollectionId      string            `yaml:"collection-id,omitempty" mapstructure:"collection-id,omitempty" json:"collection-id,omitempty"`
-	TraceTTL          time.Duration     `yaml:"trace-ttl,omitempty" mapstructure:"trace-ttl,omitempty" json:"trace-ttl,omitempty"`
-	ShortListedTraces ConspicuousTraces `yaml:"conspicuous-traces,omitempty" mapstructure:"conspicuous-traces,omitempty" json:"conspicuous-traces,omitempty"`
+	CollectionId      string          `yaml:"collection-id,omitempty" mapstructure:"collection-id,omitempty" json:"collection-id,omitempty"`
+	TraceTTL          time.Duration   `yaml:"trace-ttl,omitempty" mapstructure:"trace-ttl,omitempty" json:"trace-ttl,omitempty"`
+	ShortListedTraces ProminentTraces `yaml:"prominent-traces,omitempty" mapstructure:"prominent-traces,omitempty" json:"prominent-traces,omitempty"`
 }
